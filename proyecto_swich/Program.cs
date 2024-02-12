97efb34ad6a81d5dc01d9d249af85e221174f4a6
Michael112007 <117554393+Michael112007@users.noreply.github.com>
2024-02-12 08:48:50 -0400

    Agregar archivos de proyecto.

---
+++ proyecto_swich/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("operacion de suma y resta");
+
+
+
+Console.Write("elija una opcion (suma=s | resta=r)");
+char operacion = Convert.ToChar(Console.ReadLine());
+
+switch (operacion)
+{
+
+    case 's':
+        Console.WriteLine("ha seleccionado la oppcion de suma ");
+        break;
+    case 'r':
+        Console.WriteLine("ha seleccionado la oppcion de resta ");
+        break;
+    default:
+        Console.WriteLine("ha seleccionado la opcion incorrecta");
+        break;
+}
+Console.WriteLine("programa terminado.");
+
+
+
+
+
+
+
+
+ 
+
+
